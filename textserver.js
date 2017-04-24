f0639f3a167212d369a6f45855ddb6c9227bff27
john <wildfireone@gmail.com>
2017-04-24 22:52:49 +0100

    added write to text server

--- textserver.js
+++ textserver.js
@@ -1,3 +1,13 @@
+/**
+* @Author: John Isaacs <john>
+* @Date:   24-Apr-172017
+* @Filename: textserver.js
+* @Last modified by:   john
+* @Last modified time: 24-Apr-172017
+*/
+
+
+
 // content of index.js
 const http = require('http');
 const port = 80;
@@ -6,6 +16,47 @@ var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
 var mongoUrl = 'mongodb://localhost:27017/tweets';
 
+var groups = [
+'ConsFutureScot',
+'RightsSCCYP',
+'DundeeLabStu',
+'EUSNA',
+'eussps',
+'EUCUA',
+'gbinscotland',
+'GirlguidingScot',
+'YSIGlasgow',
+'GULabourClub',
+'GUTories',
+'PoliticsSociety',
+'gusna11',
+'HYouthPar',
+'LGBTYS',
+'LYScotland',
+'StirlingLY',
+'nusScotland',
+'OurGenScot',
+'scotyounggreens',
+'scotsyounglab',
+'ScoutsScotland',
+'SNPStudents',
+'SNPyouth',
+'StA_Tories',
+'stausfi',
+'StrathLabclub',
+'OfficialSYP',
+'TheBBScotland',
+'UWSLabour',
+'xchangescotland',
+'YCLScotland',
+'Yiscotland',
+'YoungScot',
+'YoungScotsUnion',
+'YouthScotland',
+'YouthLinkScot',
+'youngwomenscot',
+'CYPCS'];
+
 const requestHandler = (request, response) => {
   console.log(request.url)
   response.write('Server is up!\n');
@@ -13,10 +64,18 @@ const requestHandler = (request, response) => {
   		assert.equal(null, err);
   		response.write("Connected correctly to server \n");
 		db.collection('ge17').count(function(err, count) {
-		
+
 		response.write("we have "+count+" tweets!");
-      		db.close();
-		response.end();
+    for(var i=0; i<groups.length;i++){
+      db.collection(groups[i]).count(function(err, count) {
+        response.write(groups[i]+" has "+count+" tweets");
+        if(i == (groups.length -1)) {
+          db.close();
+          response.end();
+        }
+      });
+    }
+
 		});
 	});
 
@@ -31,4 +90,3 @@ server.listen(port, (err) => {
 
   console.log('server is listening on '+ port)
 });
-
