bf75dfba5313bcdf8f6c77e5b13b2559c8bbd6b1
john <wildfireone@gmail.com>
2017-06-06 12:49:18 +0100

    realtime output

--- endpoints.js
+++ endpoints.js
@@ -18,7 +18,7 @@ var ObjectId = require('mongodb').ObjectID;
 var mongoURL = 'mongodb://localhost:27017/tweets';
 
 
-var debateprefix = "debate_test4";
+var debateprefix = "debate_test5";
 var prefix = debateprefix;
 var trackingtag = 'ge17';
 
