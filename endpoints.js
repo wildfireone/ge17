b82dca06b119d6c62f044e15788691c624cdbcb3
john <wildfireone@gmail.com>
2017-06-08 23:00:58 +0100

    adding bubble matrix

--- endpoints.js
+++ endpoints.js
@@ -798,7 +798,7 @@ var getHashtagsForMatrix = function(response) {
     var collection = db.collection(prefix + 'debatehashcounts');
     collection.find().toArray(function(err, documents) {
       for (var i = 1; i < documents.length; i++) {
-        columns.push(i);
+        columns.push(documents[i].realtime);
         for (var j = 0; j < documents[i].counts.length; j++) {
           //console.log(documents[i].counts[j]["_id"]);
           if (documents[i].counts[j]["_id"].toLowerCase() == trackingtag.toLowerCase()) {
